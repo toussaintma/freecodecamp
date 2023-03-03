8427470114be41fd64ac56ee4ab75c2ac6b0bf0c
toussaintma <118844219+toussaintma@users.noreply.github.com>
2023-03-03 11:24:53 +0100

    Add files via upload

---
+++ Back End Development and APIs Projects/File Metadata Microservice/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+const multer  = require('multer');
+const upload = multer();
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// upload function
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  //console.log(req.file, req.body);
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
